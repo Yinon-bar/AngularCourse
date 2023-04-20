bf32612a874d48f46250397e49696c737b48f9d1
ינון בר <yinonb@hebrew-academy.org.il>
2023-04-20 15:52:34 +0300

    clean slate

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent {
-
+  public allowNewServer = false;
 }
